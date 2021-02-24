b06bcfef4bf8181646b7de168ce8d3cefe96f642
Aymeric <aymeric.delmaire@hotmail.com>
2021-02-24 12:09:17 +0100

    JS index V3 avec message d'erreur

--- test-api.js
+++ test-api.js
@@ -7,28 +7,33 @@ FETCH
 const container = document.getElementById('container-article')/*Créer une variable qui récupère mon ID du container*/
 
 fetch('http://localhost:3000/api/teddies')/*Méthode Fetch pour aller chercher l'URL de l'API*/
-    .then(res => res.json())/*Convertion des données en JSON pour les utiliser*/
-    .then(data => {/*Création de la fonction element avec forEach, pour chaque élément faire...*/
-        data.forEach(element => {
-            document.createElement("section")/*Création d'un élément section*/
-            const section = document.createElement("section")/*Mettre cette élément dans une variable section*/
-            /*Créer du HTML dans la section, création des cartes pour les nounours*/
-            section.innerHTML = 
-            `
-                <div class="card">
-                    <img class='produit' src='${element.imageUrl}' alt='Photo d'un ours en peluche' title='Ours en peluche ${element.name}'/>
-                    <h2>${element.name}</h2>
-                    <div class="card__description">
-                        <img
-                        class="rating-star"
-                        src="/images/rating_5.png"
-                        alt="Note 5 étoiles sur 5"
-                        />
-                        <p>${element.price / 100} €</p>
+    .then(res => {
+        if(res.ok){
+            res.json().then(data => {/*Convertion des données en JSON pour les utiliser*/
+            data.forEach(element => {
+                document.createElement("section")/*Création d'un élément section*/
+                const section = document.createElement("section")
+                section.innerHTML = 
+                `
+                    <div class="card">
+                        <img class='produit' src='${element.imageUrl}' alt='Photo d'un ours en peluche' title='Ours en peluche ${element.name}'/>
+                        <h2>${element.name}</h2>
+                        <div class="card__description">
+                            <img
+                            class="rating-star"
+                            src="/images/rating_5.png"
+                            alt="Note 5 étoiles sur 5"
+                            />
+                            <p>${element.price / 100} €</p>
+                        </div>
+                        <a href="/view/menu/produit.html">Commander</a>
                     </div>
-                    <a href="/view/menu/produit.html">Commander</a>
-                </div>
-            `
+                `
             container.append(section)/*Permet d'insérer un ensemble d'objets dans la variable section*/
-        });
+            })
+        })
+        } else {
+            console.log("ERROR")
+            document.getElementById('erreur').innerHTML = "<strong>Erreur de chargement</strong>"
+        }
     })
